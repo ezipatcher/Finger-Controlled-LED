913d381fc0d107cd09957f5ab70f7c47a34660bc
Rupam Gope <9681rupam@gmail.com>
2026-08-17 12:37:45 +0530

    Create Sketch.ino

---
+++ Sketch.ino
@@ -0,0 +1,20 @@
+const int LED_PIN = 13;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+
+    if (command == '1') {
+      digitalWrite(LED_PIN, HIGH);
+    }
+
+    if (command == '0') {
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+}
